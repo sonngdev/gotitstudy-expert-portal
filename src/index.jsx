7caeb36483054bef6e40281eeab677d0fe9d767f
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-01 18:32:21 +0700

    Import design system css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+import '@gotitinc/design-system/dist/index.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));

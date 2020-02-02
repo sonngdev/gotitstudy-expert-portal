8918905477034ada35b066118d8c746853729836
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-02 16:06:52 +0700

    Create StopWorkingButton

---
+++ src/components/TopBar/StopWorkingButton.jsx
@@ -0,0 +1,35 @@
+import React, { useState, Fragment } from 'react';
+import { Button, Modal } from '@gotitinc/design-system';
+
+export function StopWorkingButton() {
+	const [ modalVisible, setModalVisible ] = useState(false);
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
+  const stopWorking = () => window.location.reload();
+
+	return (
+		<Fragment>
+			<Button variant="white_outline" className="stop-working" onClick={showModal}>
+				<Button.Label>Stop working</Button.Label>
+			</Button>
+
+      <Modal show={modalVisible} size="small" onHide={hideModal}>
+				<Modal.Header closeButton={true}>
+					<Modal.Title>Stop working</Modal.Title>
+				</Modal.Header>
+
+				<Modal.Body>Are you sure you want to stop working?</Modal.Body>
+
+				<Modal.Footer className="u-flex">
+          <Button variant="secondary" className="cancel" onClick={hideModal}>
+            <Button.Label>Cancel</Button.Label>
+          </Button>
+
+					<Button variant="primary" className="confirm" onClick={stopWorking}>
+            <Button.Label>Stop working</Button.Label>
+          </Button>
+				</Modal.Footer>
+			</Modal>
+		</Fragment>
+	);
+}
---
+++ src/components/TopBar/StopWorkingButton.test.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { StopWorkingButton } from './StopWorkingButton';
+
+describe('<StopWorkingButton />', () => {
+  let wrapper;
+  let button;
+
+  beforeEach(() => {
+    wrapper = shallow(<StopWorkingButton />);
+    button = wrapper.find('.stop-working').first();
+  });
+
+  it('renders', () => {
+    const r = () => shallow(<StopWorkingButton />);
+    expect(r).not.toThrow();
+  });
+
+  it('contains a button', () => {
+    expect(button.exists()).toBe(true);
+    expect(button.text()).toBe('Stop working');
+  });
+
+  it('has a modal that is initially hidden', () => {
+    const modal = wrapper.find('Modal');
+    expect(modal.prop('show')).toBe(false);
+  });
+
+  describe('<Modal />', () => {
+    let modal;
+
+    beforeEach(() => {
+      modal = wrapper.find('Modal');
+      button.simulate('click');
+    });
+
+    it.skip('shows up when button is clicked', () => {
+      expect(modal.prop('show')).toBe(true);
+    });
+
+    it('hides when Cancel button is clicked', () => {
+      const cancel = modal.find('.cancel');
+      cancel.simulate('click');
+      expect(modal.prop('show')).toBe(false);
+    });
+
+    it('reloads the page when Confirm button is clicked', () => {
+      const { location } = window;
+      delete window.location;
+      window.location = { reload: jest.fn() };
+
+      const confirm = modal.find('.confirm');
+      confirm.simulate('click');
+      expect(window.location.reload).toHaveBeenCalled();
+
+      window.location = location;
+    });
+  });
+
+});

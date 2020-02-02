1bffb9696be51b5313a44c07d208e3affafe73e6
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-02 17:31:01 +0700

    Write a helper that format countdown timer

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,10 @@ import moment from 'moment-timezone';
 export function getCstTime() {
   return moment().tz('America/Chicago').format('MMM D, YYYY HH:mm:ss z');
 }
+
+export function formatCountdownTimer(milisecond) {
+  const minute = Math.floor(milisecond / 1000 / 60);
+  const second = Math.floor(milisecond / 1000) - 60 * minute;
+
+  return [minute, second].map(n => String(n).padStart(2, '0')).join(':');
+}

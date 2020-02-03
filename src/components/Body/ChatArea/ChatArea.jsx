dbb53cdf95225ba85e46a7a4b0ee3ab9453db4ba
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-03 17:39:45 +0700

    feat: render initial state of ChatArea

--- src/components/Body/ChatArea/ChatArea.jsx
+++ src/components/Body/ChatArea/ChatArea.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { BubbleChat } from '@gotitinc/design-system';
 
 export function ChatArea() {
+  const messages = useSelector((state) => state.chatMessages);
+  const messageBubbles = messages.map(({ type, avatar, text }, i) => (
+    <BubbleChat type={type} avatar={avatar} text={text} key={i} />
+  ));
+
   return (
-    <div>chat area</div>
+    <>
+      {messageBubbles}
+    </>
   );
 }

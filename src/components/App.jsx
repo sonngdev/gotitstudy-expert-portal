0e5b66edfe03d093f9b3b329021e8279c9768273
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-03 12:02:53 +0700

    feat: App is now a named export

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { TopBar } from './TopBar';
 
-function App() {
+export function App() {
   return (
     <TopBar />
   );
 }
-
-export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { App } from './components/App';
 import './styles/index.css';
 import '@gotitinc/design-system/dist/index.min.css';
 

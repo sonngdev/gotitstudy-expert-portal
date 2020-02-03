d63267c3ba1f19b8784cf4685a131355623a5718
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-03 17:39:09 +0700

    feat: inject store to App

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import { App } from './components/App';
 import './styles/index.css';
 import '@gotitinc/design-system/dist/index.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);

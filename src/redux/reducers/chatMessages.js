617b003d0f4f2292e4a97863a389de40a1dd6785
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-04 11:42:24 +0700

    feat: move initial chat messages to store folder

--- src/redux/reducers/chatMessages.js
+++ src/redux/reducers/chatMessages.js
@@ -1,16 +1,5 @@
-const initialState = [
-  {
-    type: 'reviser',
-    avatar: 'asker',
-    text: 'Please help me',
-  },
-  {
-    type: 'system',
-    avatar: 'gotit',
-    text: 'GotIt! monitors chat for the safety of our community. Never share any personal or private information. Please use the flag button to notify the GotIt! Team of inapporpriate behavior.',
-  },
-];
+import { chatMessages as initialChatMessages } from '../store/chatMessages';
 
-export function chatMessages(state = initialState, action) {
+export function chatMessages(state = initialChatMessages, action) {
   return state;
 }
---
+++ src/redux/store/chatMessages.js
@@ -0,0 +1,12 @@
+export const chatMessages = [
+  {
+    type: 'reviser',
+    avatar: 'asker',
+    text: 'Please help me',
+  },
+  {
+    type: 'system',
+    avatar: 'gotit',
+    text: 'GotIt! monitors chat for the safety of our community. Never share any personal or private information. Please use the flag button to notify the GotIt! Team of inapporpriate behavior.',
+  },
+];

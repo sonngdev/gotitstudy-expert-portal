ce47b908e4fa6334f656af88d59e0954560218ae
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-04 17:59:05 +0700

    test: improve ChatMessages test

--- src/components/Body/ChatArea/__tests__/ChatMessages.test.jsx
+++ src/components/Body/ChatArea/__tests__/ChatMessages.test.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import configureStore from 'redux-mock-store';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
-import { chatMessages as initialChatMessages } from '../../../../redux/store/chatMessages'
-import { chatMessages } from '../../../../redux/store/chatMessages';
+import { store } from '../../../../redux/store';
 import { ChatMessages } from '../ChatMessages';
 
 describe('<ChatMessages />', () => {
   let wrapper;
-  const store = configureStore()({ chatMessages });
+  const messages = store.getState().chatMessages;
 
   beforeEach(() => {
     wrapper = mount(
@@ -20,12 +18,12 @@ describe('<ChatMessages />', () => {
 
   it('renders all messages in the conversation', () => {
     const chats = wrapper.find('BubbleChat');
-    expect(chats.length).toBe(initialChatMessages.length);
+    expect(chats.length).toBe(messages.length);
   });
 
   it('contains initial messages from asker and system', () => {
-    const askerMsg = initialChatMessages.find((msg) => msg.type === 'reviser');
-    const systemMsg = initialChatMessages.find((msg) => msg.type === 'system');
+    const askerMsg = messages.find((msg) => msg.type === 'reviser');
+    const systemMsg = messages.find((msg) => msg.type === 'system');
 
     expect(wrapper.text()).toContain(askerMsg.text);
     expect(wrapper.text()).toContain(systemMsg.text);

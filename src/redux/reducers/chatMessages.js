2f52e766890ba8f5b50920f34764a59a51bc27f9
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-04 15:37:53 +0700

    feat: handle adding action in chat messages reducer

--- src/redux/reducers/chatMessages.js
+++ src/redux/reducers/chatMessages.js
@@ -1,5 +1,9 @@
 import { chatMessages as initialChatMessages } from '../store/chatMessages';
+import { CHAT_MESSAGES } from '../actions/chatMessages';
 
 export function chatMessages(state = initialChatMessages, action) {
-  return state;
+  switch (action.type) {
+    case CHAT_MESSAGES.ADD: return [...state, action.message];
+    default: return state;
+  }
 }

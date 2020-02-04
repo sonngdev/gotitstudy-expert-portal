118a510f82218f9d62532d3c16b7cf9468b7fa7d
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-04 17:32:52 +0700

    feat: create ChatInput, wire up with redux

---
+++ src/components/Body/ChatArea/ChatInput.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { chatMessagesAdd } from '../../../redux/actions/chatMessages';
+import { Button, Form } from '@gotitinc/design-system';
+
+export function ChatInput() {
+  const [ input, setInput ] = useState('');
+  const dispatch = useDispatch();
+  const bindInput = (e) => setInput(e.target.value);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    const msg = { type: 'sender', avatar: 'expert', text: input };
+    const action = chatMessagesAdd(msg);
+    dispatch(action);
+    setInput('');
+  }
+
+  return (
+    <Form onSubmit={sendMessage}>
+      <Form.InputGroup>
+        <Form.Input placeholder="Message..." name="message" value={input} onChange={bindInput} />
+        <Form.InputGroup.Append className="u-flex">
+          <Button>
+            <Button.Label>Send</Button.Label>
+          </Button>
+        </Form.InputGroup.Append>
+      </Form.InputGroup>
+    </Form>
+  );
+}

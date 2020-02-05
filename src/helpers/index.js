c045a881131e380ee8a52b1be002d5001556f389
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-05 17:23:09 +0700

    style: adhere to arrow-parens

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -9,5 +9,5 @@ export function formatCountdownTimer(milisecond) {
   const minute = Math.floor(milisecond / 1000 / 60);
   const second = Math.floor(milisecond / 1000) - 60 * minute;
 
-  return [minute, second].map(n => String(n).padStart(2, '0')).join(':');
+  return [minute, second].map((n) => String(n).padStart(2, '0')).join(':');
 }

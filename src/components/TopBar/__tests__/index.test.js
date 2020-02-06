fe3906197e9f0fa6a6a44bf830fda6c765e7bbbe
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-06 18:57:18 +0700

    feat: put TopBar module code inside index.js

--- src/components/TopBar/__tests__/index.test.js
+++ src/components/TopBar/__tests__/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { TopBar } from '../TopBar';
+import { TopBar } from '../index';
 
 describe('<TopBar />', () => {
   let wrapper;
--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,1 +1,27 @@
-export { TopBar } from './TopBar';
+import React from 'react';
+import { TopMenu, Logo } from '@gotitinc/design-system';
+import { ExpertRanking } from './ExpertRanking';
+import { WorkingStats } from './WorkingStats';
+import { CurrentTime } from './CurrentTime';
+import { StopWorkingButton } from './StopWorkingButton';
+import { SessionTimer } from './SessionTimer';
+import './styles/index.css';
+
+export function TopBar() {
+  return (
+    <TopMenu className="u-backgroundPrimary u-paddingVerticalTiny u-paddingHorizontalMedium u-textWhite">
+      <Logo name="gotit" variant="white" />
+
+      <ExpertRanking />
+
+      <WorkingStats />
+
+      <SessionTimer />
+
+      <div>
+        <CurrentTime />
+        <StopWorkingButton />
+      </div>
+    </TopMenu>
+  );
+}

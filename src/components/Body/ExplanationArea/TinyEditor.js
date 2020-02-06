06ce582a9670dd2ef14b722ea42a671aec546a6e
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-06 17:40:19 +0700

    refactor: extract custom hook from TinyEditor

--- src/components/Body/ExplanationArea/TinyEditor.js
+++ src/components/Body/ExplanationArea/TinyEditor.js
@@ -1,44 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import tinymce from 'tinymce';
-import 'tinymce/themes/silver';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/lists';
-import 'tinymce/plugins/tiny_mce_wiris';
+import React, { useState } from 'react';
+import { useTinyMceEditor } from '../../../hooks';
 
 /**
  * Custom tinymce React component:
  * https://www.tiny.cloud/blog/how-to-integrate-react-with-tinymce/
  */
 export function TinyEditor({ id, onChange }) {
-  const [editor, setEditor] = useState(null);
   const [input, setInput] = useState('');
   const bindAndCall = (value) => {
     setInput(value);
     onChange(value);
   };
 
-  useEffect(() => {
-    tinymce.init({
-      selector: `#${id}`,
-      /**
-       * Convenience aside, the `auto_focus` attribute is required to avoid a
-       * Mathtype bug: if user tries to insert a formula when the editor has
-       * not been focused (once is enough), an error is thrown.
-       */
-      auto_focus: id,
-      skin_url: `${process.env.PUBLIC_URL}/skins/ui/oxide`,
-      menubar: false,
-      branding: false,
-      plugins: 'link lists tiny_mce_wiris',
-      toolbar: 'bold italic underline strikethrough bullist numlist link tiny_mce_wiris_formulaEditor',
-      setup: (ed) => {
-        setEditor(ed);
-        ed.on('keyup change', () => bindAndCall(ed.getContent()));
-      },
-    });
-
-    return () => tinymce.remove(editor);
-  }, []);
+  useTinyMceEditor(id, bindAndCall);
 
   return (
     <textarea id={id} value={input} onChange={(e) => bindAndCall(e.target.value)} />
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,8 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import tinymce from 'tinymce';
+import 'tinymce/themes/silver';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/tiny_mce_wiris';
 
 export function useScrollToBottom(windowRef) {
   useEffect(() => {
     const wndw = windowRef.current;
     wndw.scrollTop = wndw.scrollHeight - wndw.clientHeight;
   });
+}
+
+export function useTinyMceEditor(id, onChange) {
+  const [editor, setEditor] = useState(null);
+
+  useEffect(() => {
+    tinymce.init({
+      selector: `#${id}`,
+      /**
+       * Convenience aside, the `auto_focus` attribute is required to avoid a
+       * Mathtype bug: if user tries to insert a formula when the editor has
+       * not been focused (once is enough), an error is thrown.
+       */
+      auto_focus: id,
+      skin_url: `${process.env.PUBLIC_URL}/skins/ui/oxide`,
+      menubar: false,
+      branding: false,
+      plugins: 'link lists tiny_mce_wiris',
+      toolbar: 'bold italic underline strikethrough bullist numlist link tiny_mce_wiris_formulaEditor',
+      setup: (ed) => {
+        setEditor(ed);
+        ed.on('keyup change', () => onChange(ed.getContent()));
+      },
+    });
+
+    return () => tinymce.remove(editor);
+  }, []);
 }

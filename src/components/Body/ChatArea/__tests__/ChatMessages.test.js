b93aa777b41b542283618bc8a3c71047544e5dbf
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-11 23:28:02 +0700

    test: use mock variable in ChatMessages

--- src/components/Body/ChatArea/__tests__/ChatMessages.test.js
+++ src/components/Body/ChatArea/__tests__/ChatMessages.test.js
@@ -3,25 +3,30 @@ import { shallow } from 'enzyme';
 import { useChatMessagesList, useScrollToBottom } from '../../../../utils';
 import { ChatMessages } from '../ChatMessages';
 
+/**
+ * Prefix variable with `mock` so it gets hoisted along with `jest.mock`
+ */
+const mockMessages = [
+  {
+    type: 'reviser',
+    avatar: 'asker',
+    text: 'How do you do this?',
+  },
+  {
+    type: 'system',
+    avatar: 'gotit',
+    text: 'This is a system message from Got It!.',
+  },
+  {
+    type: 'sender',
+    avatar: 'expert',
+    text: 'Let me take a look.',
+  },
+];
+
 jest.mock('../../../../utils', () => ({
   __esModule: true,
-  useChatMessagesList: jest.fn(() => [
-    {
-      type: 'reviser',
-      avatar: 'asker',
-      text: 'How do you do this?',
-    },
-    {
-      type: 'system',
-      avatar: 'gotit',
-      text: 'This is a system message from Got It!.',
-    },
-    {
-      type: 'sender',
-      avatar: 'expert',
-      text: 'Let me take a look.',
-    },
-  ]),
+  useChatMessagesList: jest.fn(() => mockMessages),
   useScrollToBottom: jest.fn(),
 }));
 
@@ -42,8 +47,8 @@ describe('<ChatMessages />', () => {
 
   it('renders all messages in the conversation in order', () => {
     const chats = wrapper.find('BubbleChat');
-    expect(chats.length).toBe(3);
-    expect(chats.at(0).prop('type')).toBe('reviser');
-    expect(chats.at(2).prop('type')).toBe('sender');
+    expect(chats.length).toBe(mockMessages.length);
+    expect(chats.at(0).prop('text')).toBe(mockMessages[0].text);
+    expect(chats.at(2).prop('text')).toBe(mockMessages[2].text);
   });
 });

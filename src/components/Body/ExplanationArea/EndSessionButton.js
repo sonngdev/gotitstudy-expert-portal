495ed6a0212339af1af1a7d0968d00a58cc3480c
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-15 20:51:08 +0700

    feat: add `className` prop to EndSessionButton

--- src/components/Body/ExplanationArea/EndSessionButton.js
+++ src/components/Body/ExplanationArea/EndSessionButton.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ButtonWithModal } from '../../shared';
 
-export function EndSessionButton() {
+export function EndSessionButton({ className }) {
   return (
     <ButtonWithModal
-      buttonProps={{ className: 'end-session' }}
+      buttonProps={{ className: `end-session ${className}` }}
       buttonAction="End session"
       modalBody="Are you sure you want to end this session? This action cannot be undone."
       onConfirm={() => window.location.reload()}
     />
   );
+}
+
+EndSessionButton.propTypes = {
+  className: PropTypes.string,
+};
+
+EndSessionButton.defaultProps = {
+  className: '',
 }

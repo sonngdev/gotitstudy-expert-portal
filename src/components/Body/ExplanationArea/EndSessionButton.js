3224f625336e901c598e4216743e520e88b2f110
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-15 20:04:37 +0700

    refactor: EndSessionButton now uses ButtonWithModal

--- src/components/Body/ExplanationArea/EndSessionButton.js
+++ src/components/Body/ExplanationArea/EndSessionButton.js
@@ -1,34 +1,13 @@
-import React, { useState } from 'react';
-import { Button, Modal } from '@gotitinc/design-system';
+import React from 'react';
+import { ButtonWithModal } from '../../shared';
 
 export function EndSessionButton() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const showModal = () => setModalVisible(true);
-  const hideModal = () => setModalVisible(false);
-  const endSession = () => window.location.reload();
-
   return (
-    <>
-      <Button className="end-session" onClick={showModal}>End session</Button>
-
-      <Modal size="small" show={modalVisible}>
-        <Modal.Header closeButton>
-          <Modal.Title>Stop working</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          Are you sure you want to end this session? This action cannot be undone.
-        </Modal.Body>
-
-        <Modal.Footer className="u-flex">
-          <Button variant="secondary" className="cancel" onClick={hideModal}>
-            Cancel
-          </Button>
-          <Button variant="primary" className="confirm" onClick={endSession}>
-            Stop working
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <ButtonWithModal
+      buttonProps={{ className: 'end-session' }}
+      buttonAction="End session"
+      modalBody="Are you sure you want to end this session? This action cannot be undone."
+      onConfirm={() => window.location.reload()}
+    />
   );
 }

5b8f8309a9380fe656c7ef52dc7db5befcc4b481
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-02-15 19:44:27 +0700

    refactor: StopWorkingButton now use ButtonWithModal

--- src/components/TopBar/StopWorkingButton.js
+++ src/components/TopBar/StopWorkingButton.js
@@ -1,41 +1,18 @@
-import React, { useState } from 'react';
-import { Button, Modal } from '@gotitinc/design-system';
+import React from 'react';
+import { ButtonWithModal } from '../shared';
 
 export function StopWorkingButton() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const showModal = () => setModalVisible(true);
-  const hideModal = () => setModalVisible(false);
-  const stopWorking = () => window.location.reload();
-
   return (
-    <>
-      <Button
-        variant="white_outline"
-        size="small"
-        width="full"
-        className="stop-working"
-        onClick={showModal}
-      >
-        <Button.Label>Stop working</Button.Label>
-      </Button>
-
-      <Modal show={modalVisible} size="small" onHide={hideModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Stop working</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>Are you sure you want to stop working?</Modal.Body>
-
-        <Modal.Footer className="u-flex">
-          <Button variant="secondary" className="cancel" onClick={hideModal}>
-            <Button.Label>Cancel</Button.Label>
-          </Button>
-
-          <Button variant="primary" className="confirm" onClick={stopWorking}>
-            <Button.Label>Stop working</Button.Label>
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <ButtonWithModal
+      buttonProps={{
+        variant: 'white_outline',
+        size: 'small',
+        width: 'full',
+        className: 'stop-working',
+      }}
+      buttonAction="Stop working"
+      modalBody="Are you sure you want to stop working?"
+      onConfirm={() => window.location.reload()}
+    />
   );
 }
--- src/components/TopBar/__tests__/StopWorkingButton.test.js
+++ src/components/TopBar/__tests__/StopWorkingButton.test.js
@@ -4,51 +4,12 @@ import { StopWorkingButton } from '../StopWorkingButton';
 
 describe('<StopWorkingButton />', () => {
   let wrapper;
-  let button;
 
   beforeEach(() => {
     wrapper = shallow(<StopWorkingButton />);
-    button = wrapper.find('.stop-working').first();
   });
 
   it('renders', () => {
     expect(wrapper.debug()).toMatchSnapshot();
-  });
-
-  it('contains a button', () => {
-    expect(button.exists()).toBe(true);
-    expect(button.text()).toBe('Stop working');
-  });
-
-  it('has a modal that is initially hidden', () => {
-    const modal = wrapper.find('Modal');
-    expect(modal.prop('show')).toBe(false);
-  });
-
-  describe('<Modal />', () => {
-    beforeEach(() => {
-      button.simulate('click');
-    });
-
-    it('shows up when button is clicked', () => {
-      expect(wrapper.find('Modal').prop('show')).toBe(true);
-    });
-
-    it('hides when Cancel button is clicked', () => {
-      wrapper.find('Modal').find('.cancel').simulate('click');
-      expect(wrapper.find('Modal').prop('show')).toBe(false);
-    });
-
-    it('reloads the page when Confirm button is clicked', () => {
-      const { location } = window;
-      delete window.location;
-      window.location = { reload: jest.fn() };
-
-      const confirm = wrapper.find('Modal').find('.confirm');
-      confirm.simulate('click');
-      expect(window.location.reload).toHaveBeenCalled();
-
-      window.location = location;
-    });
   });
 });
